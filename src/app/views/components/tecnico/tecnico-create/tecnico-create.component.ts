f520bff1a977ee387edb629b5a84837be30326e9
WdvOps <dvops@outlook.com>
2021-09-27 20:34:57 -0300

    Bug:  Tratamento de excessões não captura(parcialmente) mensagem do backend

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,13 +10,37 @@ import { Router } from '@angular/router';
 })
 export class TecnicoCreateComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  tecnico: Tecnico = {
+    id: '',
+    nome: 'Agatha Monteiro',
+    cpf: '0435.325.297-69',
+    telefone: '(21) 2502-4944'
+  }
+
+  constructor(
+    private router : Router,
+    private service: TecnicoService) { }
 
   ngOnInit(): void {
   }
 
   cancel():void {
     this.router.navigate(['tecnicos'])
+  }
+
+  create():void {
+    this.service.create(this.tecnico).subscribe((resposta) => {
+      this.router.navigate(['tecnicos'])
+      this.service.message('Cadastro de técnico realizado com sucesso!')
+    }, err => {
+      
+      if(err.error.error.match('já cadastrado')) {
+        this.service.message(err.error.error)
+      } else if(err.error.errors[0].message === "número do registro de contribuite individual brasileiro (CPF) inválido") {
+        this.service.message(err.error.errors[0].message)
+      }
+      console.log(err)
+    })
   }
 
 }
